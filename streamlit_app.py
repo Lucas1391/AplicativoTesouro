3660f40b448d2e0fe2a8132eec228d2b6936fdbb
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-06-04 16:19:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import streamlit as st
 lista_name = ["Lucas Campos"]
 lista_senha = ["lucasomatoria7@gmail.com"]
 st.title("Login de inicialização")
-name = st.text_input("Digite seu login : ")
-senha = st.text_input("Digite sua senha : ")
-if st.button("Efetue Login"):
-    if (name in lista_name)and( senha in lista_senha):
+usuario = st.sidebar.text_input("Insira seu nome de usuário : ")
+senha = st.sidebar.text_input("Insira sua senha : ",type='password')
+if senha:
+    if (usuario in lista_name)and( senha in lista_senha):
         st.text("Seja bem vindo!")
     else:
         st.text("Falha no login")
