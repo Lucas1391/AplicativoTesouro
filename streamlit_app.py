b7443bdd10a297587a1e6579e3f629b00fbb8140
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-06-05 20:37:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,29 @@
 import streamlit as st
 
-lista_name = ["Lucas Campos"]
-lista_senha = ["lucasomatoria7@gmail.com"]
-st.title("Login de inicialização")
-usuario = st.text_input("Insira seu nome de usuário : ")
-senha = st.text_input("Insira sua senha : ",type='password')
-if senha:
-    if (usuario  not in lista_name)and(senha not in lista_senha):
+def main_page():
+    st.markdown("# Main page 🎈")
+    usuario = st.text_input("Insira seu nome de usuário : ")
+    senha = st.text_input("Insira sua senha : ",type='password')
+    if senha:
+        if (usuario  not in lista_name)and(senha not in lista_senha):
         st.text("Falha no Login!")
     else:
         st.text("Seja bem vindo!")
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+page_names_to_funcs = {
+    "Main Page": Login,
+    "Page 2": Home,
+
+}
+lista_name = ["Lucas Campos"]
+lista_senha = ["lucasomatoria7@gmail.com"]
+
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
+
+        
